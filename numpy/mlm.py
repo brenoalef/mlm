15a51738cf2a7aed00dcb8132698c11fa284ee67
Jessé Lima Sá <b.alefdsa@gmail.com>
2019-03-17 10:48:27 -0300

    Model selection

--- numpy/mlm.py
+++ numpy/mlm.py
@@ -5,7 +5,8 @@ from numpy.matlib import repmat
 
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import pairwise_distances
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, LeaveOneOut
+
 from sklearn.metrics.pairwise import euclidean_distances
 
 from scipy.optimize import root
@@ -40,6 +41,7 @@ class MLM:
         #Dy = euclidean_distances(Y, self.T)
         #self.B_hat = np.linalg.pinv(Dx).dot(Dy)
         self.B_hat = np.linalg.pinv(euclidean_distances(X, self.R)).dot(euclidean_distances(Y, self.T))
+        return self
 
     def predict(self, X, Y=None, method="nn"):
         #Dx = euclidean_distances(X, self.R)
@@ -78,3 +80,27 @@ def get_amse(X, Y, model):
     Dy = euclidean_distances(Y, model.T)
     errors = Dy - Dyh
     return np.mean(np.square(errors))
+
+
+def model_selection(X, Y, k_values=[], n_folds=10, srp="rand"):
+    N = X.shape[0]
+    if n_folds == N:
+        CVO = LeaveOneOut()
+    else:
+        CVO = KFold(n_splits=n_folds)
+
+    amse_values = []
+
+    for train_index, test_index in CVO.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        Y_train, Y_test = Y[train_index], Y[test_index]
+        
+        amse_k = []
+        for j in range(len(k_values)):
+            model = MLM()
+            model.train(X_train, Y_train, k=k_values[j], srp=srp)
+            amse_k.append(get_amse(X_test, Y_test, model))
+        amse_values.append(amse_k)
+    
+    ecv = np.mean(np.array(amse_values), axis=0)
+    return k_values[np.argmin(ecv)] 
